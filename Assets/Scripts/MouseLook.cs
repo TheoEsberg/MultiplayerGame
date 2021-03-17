1e6c94a1c011649117f89517e37f99c876601d0c
Theo Esberg <esbergtheo@gmail.com>
2021-03-17 13:45:01 +0100

    Update v0.1

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensativity = 500f;
+    public Transform playerBody;
+
+    private float xRotation = 0f;
+
+    private void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update() {
+        LookAround();
+
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
+    private void LookAround(){
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensativity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensativity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
